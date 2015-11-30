0e6904eb19ff79a76c47531fa4ac77758ee84b0c
mustafiz012 <mustafiz_kuet_cse@yahoo.com>
2015-11-30 22:34:00 +0600

    30th November'15

---
+++ nothingImportant.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char array[200][300] = {"amr", "sonar", "bangla", "ami"};
+    int i =0;
+    while(array!=NULL){
+        printf("%s ", array[i]);
+        i++;
+    }
+}
